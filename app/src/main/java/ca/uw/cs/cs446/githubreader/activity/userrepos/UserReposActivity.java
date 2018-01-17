c637b63ec2ac8440c2c759e06228a593306be26b
Cassiano Monteiro <cassianomonteiro@gmail.com>
2018-01-16 20:35:37 -0500

    Added progress bar. Set title on onCreate.

--- app/src/main/java/ca/uw/cs/cs446/githubreader/activity/userrepos/UserReposActivity.java
+++ app/src/main/java/ca/uw/cs/cs446/githubreader/activity/userrepos/UserReposActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import java.util.List;
 
@@ -18,21 +20,28 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class UserReposActivity extends AppCompatActivity {
 
+    private String username;
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_repos);
+
+        // Get a private reference to the progress bar
+        progressBar = findViewById(R.id.progressBar);
+
+        // Get the piece of data passed to this screen and store in a private variable
+        username = getIntent().getStringExtra("USERNAME");
+
+        // Set the title on the top bar at the screen
+        // We want this in onCreate to make sure it appears when the screen is shown to the user
+        setTitle(username);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
-        // Get the piece of data passed to this screen
-        String username = getIntent().getStringExtra("USERNAME");
-
-        // Set the title on the top bar at the screen
-        setTitle(username);
 
         // Create a Retrofit instance to handle HTTP requests
         Retrofit retrofit = new Retrofit.Builder()
@@ -46,8 +55,14 @@ public class UserReposActivity extends AppCompatActivity {
         // Create a call to execute the HTTP request
         Call<List<UserRepo>> userReposCall = service.getUserRepos(username);
 
-        // Calling "execute" method runs it synchronously.
-        // This will cause NetworkOnMainThreadException
+        // Show the progress while waiting for request.
+        // We show it before starting the request.
+        progressBar.setVisibility(View.VISIBLE);
+
+        /**
+         * Calling "execute" method runs it synchronously.
+         * This will cause NetworkOnMainThreadException
+         */
 //        try {
 //            Response<List<UserRepo>> response = userReposCall.execute();
 //            RecyclerView reposRecyclerView = findViewById(R.id.reposRecyclerView);
@@ -63,6 +78,9 @@ public class UserReposActivity extends AppCompatActivity {
             // This method will be called on a successful response from the HTTP request
             @Override
             public void onResponse(Call<List<UserRepo>> call, Response<List<UserRepo>> response) {
+
+                // Hide the progress bar as the request is completed
+                progressBar.setVisibility(View.INVISIBLE);
 
                 // Get the data returned by the HTTP request
                 List<UserRepo> userRepos = response.body();
@@ -84,6 +102,9 @@ public class UserReposActivity extends AppCompatActivity {
             // This method will be called on a FAILED response from the HTTP request
             @Override
             public void onFailure(Call<List<UserRepo>> call, Throwable t) {
+                // Hide the progress bar as the request is completed
+                // Would be nice to show some error message to the user
+                progressBar.setVisibility(View.INVISIBLE);
             }
         });
 
